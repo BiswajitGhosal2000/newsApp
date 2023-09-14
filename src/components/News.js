50cceea13e81fd4c6399d965c81fd19c161ee531
Acer <ghosalbiswajit11@gmail.com>
2023-09-14 09:49:26 +0530

    Initial Commit

---
+++ src/components/News.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+
+export class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { articles: [], loading: false, page: 1 };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=8a6c80eda7fc42388b12954d0e59aeed&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  }
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=8a6c80eda7fc42388b12954d0e59aeed&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
+  };
+  handleNextClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=8a6c80eda7fc42388b12954d0e59aeed&page=${
+      this.state.page + 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
+  };
+  render() {
+    return (
+      <div className="container my-3">
+        <h2 className="text-center">{this.props.headline}</h2>
+        {this.state.loading && <Spinner />}
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 88) : ""
+                  }
+                  imgUrl={
+                    element.urlToImage
+                      ? element.urlToImage
+                      : "https://t3.ftcdn.net/jpg/03/27/55/60/360_F_327556002_99c7QmZmwocLwF7ywQ68ChZaBry1DbtD.jpg"
+                  }
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between w-75">
+          <button
+            type="button"
+            disabled={this.state.page <= 1}
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            disabled={
+              !Math.ceil(
+                this.state.page < this.state.totalResults / this.props.pageSize
+              )
+            }
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imgUrl, newsUrl } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            className="card-img-top"
+            src={imgUrl}
+            alt="Card cap"
+            style={{ width: "18rem", height: "15rem" }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}....</h5>
+            <p className="card-text">{description}....</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
